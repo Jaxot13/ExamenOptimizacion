77bf0b70749575a0d70429bb49b7d2cd27ad174f
Jaxot13 <jorgesanru@gmail.com>
2025-06-30 20:10:49 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function info(artista, disco, nCanciones, ventas){
+    alert ("Artista: " + artista + "\nDisco: " + disco + "\nNúmero de canciones: " + nCanciones + "\nVentas Mundiales: " + ventas + " millones");
+}
+
+function implementado(){
+    alert ("Esta función no está implementada");
+}
